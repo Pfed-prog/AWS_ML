fd36f3a2e2441f365e00b729d96799b8f9eb58c0
Pavel Fedotov <66903336+Pfed-prog@users.noreply.github.com>
2021-12-16 21:19:45 +0300

    added last timestamp and delta

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, send_from_directory, jsonify
+from flask import Flask, request, jsonify
 import requests
 import json
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 application = Flask(__name__)
@@ -25,18 +24,22 @@ def home():
         """
     r = requests.post(url, json={'query': q})
     json_data = json.loads(r.text)
+    
     df_data = json_data['data']['token']['tokenDayData']
     df = pd.DataFrame(df_data)
+
     df.priceUSD = df.priceUSD.replace(0, np.nan).dropna()
     df.priceUSD = df.priceUSD.astype(float)
 
+    last = df.date.iloc[-1:].values[0]
+    timestep = df.date.iloc[-1:].values[0]-df.date.iloc[-2:-1].values[0]
     model = ARIMA(df.priceUSD, order=(1,1,0))
     fitted = model.fit()
     # Forecast
     fc = fitted.forecast(f)
 
-    fc_series = pd.Series(fc)
-    return jsonify({'predictions': list(fc_series)})
+    #fc_series = pd.Series(fc)
+    return jsonify({'predictions': list(fc), 'last date': str(last), 'timestep': str(timestep)})
 
 if __name__ == "__main__":
     app.run(debug=True)
